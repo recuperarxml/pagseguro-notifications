727deb1f71ac6707729ca912b9c4feeb2c24ce9e
Leonardo Gregianin <leogregianin@gmail.com>
2016-10-20 00:17:35 -0300

    cleanup code

--- pagseguro.php
+++ pagseguro.php
@@ -7,10 +7,6 @@
 	// Credentials
 	$email = 'PagSeguro account';
 	$token = 'PagSeguro Token';
-	$bdServidor = 'Database address';
-	$bdUsuario  = 'Database user';
-	$bdSenha    = 'Database password';
-	$bdBanco    = 'Database name';
 
 	date_default_timezone_set('America/Sao Paulo');
 	$date = date('Y-m-d H:i');
@@ -23,11 +19,7 @@
 	$transaction = curl_exec($curl);
 	curl_close($curl);
 
-	if ($transaction == 'Unauthorized') {
-		$name = 'log1-pagseguro-receive-' . $date . '.xml';
-		$file = fopen($name, 'a');
-		fwrite($file, $transaction);
-		fclose($file);
+	if (($transaction == 'Unauthorized') or ($transaction == '')) {
 		Exit();
 	}
 	
@@ -55,72 +47,3 @@
 	$comprador_cidade      = $transaction -> shipping -> address -> city;
 	$comprador_uf          = $transaction -> shipping -> address -> state;
 	$comprador_pais        = $transaction -> shipping -> address -> country;
-	
-	// Conexão com banco de dados
-	$conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
-	if (mysqli_connect_errno($conexao)) {
-		$name = 'log2-conexao-bd-' . $date . '.txt';
-		$file = fopen($name, 'a');
-		fwrite($file, 'Erro de conexao');
-		fclose($file);
-
-		die();
-	}
-	
-	$sqlInsert = "
-	insert into transacao (
-		`comprador_email`,
-		`status`,
-		`tipo_transacao`,
-		`data_transacao`,
-		`codigo_transacao`, 
-		`tipo_pagamento`,
-		`valor_bruto`,
-		`tarifa_intermediacao`,
-		`taxa_intermediacao`, 
-		`valor_liquido`,
-		`data_credito_liberado`,
-		`comprador_nome`,
-		`comprador_telefone`, 
-		`comprador_ddd`,
-		`comprador_endereco`,
-		`comprador_numero`,
-		`comprador_bairro`, 
-		`comprador_cep`,
-		`comprador_cidade`,
-		`comprador_uf`,
-		`comprador_pais`)
-	values (
-		'$comprador_email', 
-		'$status_transaction',
-		'$tipo_transacao',
-		'$data_transacao',
-		'$codigo_transacao',
-		'$tipo_pagamento',
-		'$valor_bruto',
-		'$tarifa_intermediacao',
-		'$taxa_intermediacao',
-		'$valor_liquido',         
-		'$data_credito_liberado',
-		'$comprador_nome',
-		'$comprador_telefone',
-		'$comprador_ddd',
-		'$comprador_endereco',
-		'$comprador_numero',
-		'$comprador_bairro',
-		'$comprador_cep',
-		'$comprador_cidade',
-		'$comprador_uf',
-		'$comprador_pais'
-	)";
-
-	try {  
-		mysqli_query($conexao, $sqlInsert);
-	}  
-	catch(exception $e) {
-		$name = 'log3-insert-error-' . $date . '.txt';
-		$file = fopen($name, 'a');
-		fwrite($file, 'Erro de insert: ' .$e );
-		fclose($file);
-	}
-?>
